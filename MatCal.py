639f7c527d75a15ea84fae227359221efa8f82a9
sgao1Babson <sgao1@babson.edu>
2022-12-08 23:14:32 -0500

    Update MatCal.py

--- MatCal.py
+++ MatCal.py
@@ -9,31 +9,32 @@ class MatCal:
         self.__mat2 = mat2
 
     def add(self):
-        """This function serves for adding matrix A and B"""
+        """This function serves for doing addition of the two numbers mat1 and mat2"""
         if self.__mat1.shape != self.__mat2.shape:
             return None
         else:
             return self.__mat1 + self.__mat2
 
     def multi(self):
-        """This function serves for multiply matrix A and B"""
+        """This function serves for doing addition of the two numbers mat1 and mat2"""
         if self.__mat1.shape != self.__mat2.shape:
             return None
         return self.__mat1 * self.__mat2
 
     def matmulti(self):
+        """This function serves as multiplication of the two matrix A and B"""
         if self.__mat1.shape[1] != self.__mat2.shape[0]:
             return None
         return np.matmul(self.__mat1, self.__mat2)
 
     def sub(self):
-        """This function serves for matrix A subtrating matrix B"""
+        """This function serves for doing subtraction of the two numbers mat1 and mat2"""
         if self.__mat1.shape != self.__mat2.shape:
             return None
         return self.__mat1 - self.__mat2
 
     def divide(self):
-        """This function serves for matrix A divided by matrix B"""
+        """This function serves for doing division of the two numbers mat1 and mat2"""
         if self.__mat1.shape != self.__mat2.shape:
             return None
         return self.__mat1 / self.__mat2
